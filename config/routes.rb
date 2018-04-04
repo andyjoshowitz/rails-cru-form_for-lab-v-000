b0b58bd0f890c675d8fb215085993f1fcca7c018
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-04-04 20:26:39 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :songs
+  resources :songs
   resources :genres
   resources :artists
   # The priority is based upon order of creation: first created -> highest priority.
